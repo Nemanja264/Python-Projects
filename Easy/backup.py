4982c98d6bb853916248fe908d0e097edd07ca38
Nemanja Ilic <155994048+Nemanja264@users.noreply.github.com>
2025-03-27 18:18:24 +0100

    Add files via upload

---
+++ Easy/backup.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = r'D:\knjige'
+destination_dir = r'D:\PythonExercise\Backups'
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError as fe:
+        print(f"Folder already exists in: {dest}")
+
+copy_folder_to_directory(source_dir, destination_dir)
